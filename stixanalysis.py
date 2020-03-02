5830f88b0eb13df1a46f9d8dc71489029275f24b
tsitsi-flora <tsitsimunikwa97@gmail.com>
2020-03-02 13:01:05 +0200

    accept files as input

---
+++ stixanalysis.py
@@ -0,0 +1,21 @@
+import os
+import json
+from tkinter import filedialog
+from tkinter import *
+
+def parse_json():
+    
+    root = Tk()
+    root.filename = filedialog.askopenfilename(
+        initialdir="/", title="Select file", filetypes=(("json files", "*.json"), ("all files", "*.*")))
+    print(root.filename)
+    file_extension = os.path.splittext(root.filename) # fetch the file extension
+    if file_extension != '.json': # check if file extension is json format
+        print('INVALID FILE SELECTED.')
+    else:
+        with open(root.filename, 'r') as f:
+            json_data = json.load(f) # parsing the json file to a python object
+
+
+output = parse_json()
+print(output)
