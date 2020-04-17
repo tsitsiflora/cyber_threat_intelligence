e4a09331457b6ed984172a9c882115cc91e0214e
tsitsi-flora <tsitsimunikwa97@gmail.com>
2020-04-17 15:41:14 +0200

    system client

--- stixanalysis.py
+++ stixanalysis.py
@@ -1,5 +1,6 @@
 import os
 import json
+import stix2viz
 from tkinter import filedialog
 from tkinter import *
 
@@ -7,24 +8,17 @@ def parse_json():
     
     root = Tk()
     root.filename = filedialog.askopenfilename(
-        initialdir="/", title="Select file", filetypes=(("json files", "*.json"), ("all files", "*.*")))
+        initialdir="/Desktop", title="Select file", filetypes=(("json files", "*.json"), ("all files", "*.*")))
     print(root.filename)
-    file_extension = os.path.splittext(root.filename) # fetch the file extension
-    if file_extension != '.json': # check if file extension is json format
-        print('INVALID FILE SELECTED.')
-    else:
-        with open(root.filename, 'r') as f:
-            json_data = json.load(f) # parsing the json file to a python object
-
-
-
-import stix2viz
-import json
-
-with open('test.json', 'r') as f:
-    data = json.load(f)
-#print(data)
-stix2viz.display(data)
+    #file_extension = os.path.splittext(root.filename) # fetch the file extension
+    #if file_extension != '.json': # check if file extension is json format
+    #    print('INVALID FILE SELECTED.')
+    #else:
+    with open(root.filename, 'r') as f:
+        json_data = json.load(f) # parsing the json file to a python object
+    #print(json_data)    
+    stix2viz.display(json_data)
+        
 
 output = parse_json()
 print(output)
---
+++ taxiiclient/client/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
+]
+    
+
