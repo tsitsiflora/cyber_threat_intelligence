70588d673970785a491dd244647b9ddf0d17cb9f
tsitsi-flora <tsitsimunikwa97@gmail.com>
2020-04-17 16:21:11 +0200

    dashboard is up

--- taxiiclient/client/urls.py
+++ taxiiclient/client/urls.py
@@ -3,7 +3,8 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('login', TemplateView.as_view(template_name='index.html')),
+    path('dashboard', TemplateView.as_view(template_name='dashindex.html')),
 ]
     
 
