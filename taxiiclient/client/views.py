4fa4a2754a561f238659654a629d05d23543bc64
tsitsi-flora <tsitsimunikwa97@gmail.com>
2020-05-09 22:18:40 +0200

    add mitre attack functionality

--- taxiiclient/client/views.py
+++ taxiiclient/client/views.py
@@ -7,16 +7,16 @@ from .models import Indicator
 from stix2 import Indicator
 import json
 # Create your views here.
-  
-# Create your views here. 
-def login_view(request): 
+
+# Create your views here.
+def login_view(request):
     username = "not logged in"
-    
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(username=username, password=password)
-        
+
         if user:
             return render(request, "dashindex.html")
         else:
@@ -25,9 +25,21 @@ def login_view(request):
             return HttpResponse("Invalid login details given")
     else:
         return render(request, 'index.html', {})
-    
+
 def dashboard_view(request):
-    return render(request, 'dashindex.html')
+    server = Server("https://cti-taxii.mitre.org/taxii/")
+    api_root = server.api_roots[0]
+    
+    enteprise_collection = api_root.collections[0]
+    enteprise_objects = enteprise_collection.get_objects()
+    
+    pre_collection = api_root.collections[1]
+    pre_objects = pre_collection.get_objects()
+    
+    mobile_collection = api_root.collections[2]
+    mobile_objects = mobile_collection.get_objects()
+    
+    return render(request, 'dashindex.html', {'data1': enteprise_objects["objects"]}, {'data2': pre_objects["objects"]}, {'data3': mobile_objects["objects"]})
 
 def profile_view(request):
     return render(request, 'pages-profile.html')
@@ -36,7 +48,7 @@ def table_view(request):
     server = Server('https://limo.anomali.com/api/v1/taxii2/taxii/', user='guest', password='guest')
     api_root = server.api_roots[0]
     collection = api_root.collections[0]
-    
+
     objects = collection.get_objects()
 
     return render(request, 'table-basic.html', {'data': objects["objects"]})
@@ -44,17 +56,17 @@ def table_view(request):
 def newioc_view(request):
     #if request.method == 'POST':
     form = IndicatorForm(request.POST)
-        
+
     if form.is_valid():
         name = form.cleaned_data['name']
         labels = form.cleaned_data['labels']
         pattern = form.cleaned_data['pattern']
         indicator = Indicator(name=name, labels=labels, pattern=pattern)
         print(indicator)
-        
+
         #saving the indicator to database
         return redirect('/dashboard')
-    
+
     context = {'form':form}
-        
+
     return render(request, 'newioc.html', context)
--- test1.py
+++ test1.py
@@ -1,7 +1,13 @@
 from taxii2client.v20 import Server
-server = Server('https://limo.anomali.com/api/v1/taxii2/taxii/', user='guest', password='guest')
+server = Server("https://cti-taxii.mitre.org/taxii/")
+print(server.title)
+print(server.description)
 api_root = server.api_roots[0]
-collection = api_root.collections[0]
-objects = collection.get_objects()
-print(collection.title)
-print(objects)
+print(api_root.title)
+
+coll1 = api_root.collections[1]
+print(coll1.title)
+print(coll1.description)
+
+obj = coll1.get_objects()
+print(obj)
