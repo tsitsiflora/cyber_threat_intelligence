7b4fba18b780be8ea4dc815bba336a7cc7639ea0
tsitsi-flora <tsitsimunikwa97@gmail.com>
2020-05-21 01:26:41 +0200

    new flask api

---
+++ backend/cyberapi/app.py
@@ -0,0 +1,46 @@
+#!flask/bin/python
+#!flask/bin/python
+from flask import Flask, jsonify
+from taxii2client.v20 import Server
+
+app = Flask(__name__)
+
+
+@app.route('https://cti-taxii.mitre.org/taxii', methods=['GET'])
+def get_preattack_objects():
+    server = Server('https://cti-taxii.mitre.org/taxii')
+    api_root = server.api_roots[0]
+    collection = api_root[1]
+    
+    objects = collection.get_objects()
+    return jsonify({'data': objects["objects"]})
+
+@app.route('https://limo.anomali.com/api/v1/taxii2/taxii/', methods=['GET'])
+def get_objects():
+    server = Server('https://limo.anomali.com/api/v1/taxii2/taxii/', user='guest', password='guest')
+    api_root = server.api_roots[0]
+    collection = api_root.collections[0]
+
+    objects = collection.get_objects()
+    return jsonify({'data': objects["objects"]})
+
+@app.route('https://cti-taxii.mitre.org/taxii', methods=['GET'])
+def get_entepriseattack_objects():
+    server = Server('https://cti-taxii.mitre.org/taxii')
+    api_root = server.api_roots[0]
+    collection = api_root[0]
+    
+    objects = collection.get_objects()
+    return jsonify({'data': objects["objects"]})
+
+@app.route('https://cti-taxii.mitre.org/taxii', methods=['GET'])
+def get_mobileattack_objects():
+    server = Server('https://cti-taxii.mitre.org/taxii')
+    api_root = server.api_roots[0]
+    collection = api_root[2]
+    
+    objects = collection.get_objects()
+    return jsonify({'data': objects["objects"]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
